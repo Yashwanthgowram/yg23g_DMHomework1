308ddbe589d3b67bd39f355cfb32fc02ce409077
Yashwanthgowram <84216932+Yashwanthgowram@users.noreply.github.com>
2024-01-31 00:05:52 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -3,50 +3,40 @@ def question10_1():
     answer: python bool (True or False)
     """
     answer: bool = False
-    # Descripton : Count data can sometimes be affected by noise like measurement errors.
     return answer
 
 def question10_2():
     answer: bool = False
-    # Justification : The values isn't between the given range.
     return answer
 
 def question10_3():
     answer: bool = False
-    # Justification : Sampling is a simpler process but aggregation can be better than sampling.
     return answer
 
 def question10_4():
     answer: bool = True
-    # Justification : Noise and outliers can be sometimes the same as outliers can be noise in selective cases.
     return answer
 
 def question10_5():
     answer: bool = False
-    # Justification : Not every case of outlier can be a noise, because some outliers might be valid.
     return answer
 
 def question10_6():
     answer: bool = False
-    # Justification : Not every binary attribute can be an assymetric binary attribute because sometimes 0 or 1 can be given equal importance. 
     return answer
 
 def question10_7():
     answer: bool = True
-    # Justification : The objects are identical because their cosine similarity is 1.
     return answer
 
 def question10_8():
     answer: bool = False
-    # Justification : Discrete variables can be sometimes ratio. Like the number of cars in a parking lot (it can never be infinite)
     return answer
 
 def question10_9():
     answer: bool = False
-    # Justification : Quantitative variables can be discrete too. 
     return answer
 
 def question10_10():
     answer: bool = False
-    # Justification : There might be some loss like loss in the ordering of the variables.
     return answer
