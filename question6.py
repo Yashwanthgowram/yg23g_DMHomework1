86bf067a389f724c26192bb9c095197595eb4acd
Yashwanthgowram <84216932+Yashwanthgowram@users.noreply.github.com>
2024-01-30 23:43:59 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,14 +1,20 @@
 def question6_1():
+    """
     answer = {
-        'equal_width': ['No change', 3],
-        'equal_frequency': ['No change', 3]
+        'equal_width': ['string', integer],
+        'equal_frequency': ['string', integer]
+    }
+    """
+    answer = {
+        'equal_width': ['no change', 3],
+        'equal_frequency': ['no change', 3]
     }
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['No change', 7],
-        'equal_frequency': ['No change', 9]
+        'equal_width': ['no change', 7],
+        'equal_frequency': ['no change', 9]
     }
     return answer
 
@@ -18,3 +24,4 @@ def question6_3():
         'equal_frequency': ['no change', 9]
     }
     return answer 
+
