96dcd46e726c9f0e5123395ffb1b616d9444589a
Yashwanthgowram <84216932+Yashwanthgowram@users.noreply.github.com>
2024-01-30 23:42:20 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,49 +1,52 @@
 def question10_1():
-    answer = False
-    #Count data can be affected by noise, which may arise due to various factors such as measurement errors.
+    """
+    answer: python bool (True or False)
+    """
+    answer: bool = False
+    # Descripton : Count data can sometimes be affected by noise like measurement errors.
     return answer
 
 def question10_2():
-    answer = True
-    #A linear relationship between two sets of real values may be found with a correlation coefficient, which is a number between -1 and 1.
+    answer: bool = False
+    # Justification : The values isn't between the given range.
     return answer
 
 def question10_3():
-    answer = False
-    #Since aggregating time series data preserves more information about the trends and patterns in the data, it may be better than simple sampling.
+    answer: bool = False
+    # Justification : Sampling is a simpler process but aggregation can be better than sampling.
     return answer
 
 def question10_4():
-    answer = False
-    #While there are some similarities between noise and outliers, outliers are extreme numbers that may or may not be the result of noise.
+    answer: bool = True
+    # Justification : Noise and outliers can be sometimes the same as outliers can be noise in selective cases.
     return answer
 
 def question10_5():
-    answer = False
-    #An object being an outlier doesn't necessarily mean it is noise; it might be a legitimate extreme value in the data.
+    answer: bool = False
+    # Justification : Not every case of outlier can be a noise, because some outliers might be valid.
     return answer
 
 def question10_6():
-    answer = False
-    #An asymmetric binary attribute would have values between -1 and 1. A symmetric binary attribute has values between 0 and 1.
+    answer: bool = False
+    # Justification : Not every binary attribute can be an assymetric binary attribute because sometimes 0 or 1 can be given equal importance. 
     return answer
 
 def question10_7():
-    answer = False
-    #A cosine measure of 1 between vectors of counts indicates the objects have the same direction but may not have the same magnitudes
+    answer: bool = True
+    # Justification : The objects are identical because their cosine similarity is 1.
     return answer
 
 def question10_8():
-    answer = False
-    #If a discrete variable, has a genuine zero point, then it can be a ratio
+    answer: bool = False
+    # Justification : Discrete variables can be sometimes ratio. Like the number of cars in a parking lot (it can never be infinite)
     return answer
 
 def question10_9():
-    answer = False
-    #Either discrete or continuous variables can be quantitative; continuity is not a requirement for being quantitative.
+    answer: bool = False
+    # Justification : Quantitative variables can be discrete too. 
     return answer
 
 def question10_10():
-    answer = False
-    #Information on the ordinal relationships between categories may be lost when converting ordinal variables to asymmetric binary variables.
+    answer: bool = False
+    # Justification : There might be some loss like loss in the ordering of the variables.
     return answer
