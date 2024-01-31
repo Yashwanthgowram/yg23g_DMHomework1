c0c702890701f232ffe657a1502c42e6247c00ed
Yashwanthgowram <84216932+Yashwanthgowram@users.noreply.github.com>
2024-01-30 23:26:53 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,19 +1,19 @@
 def question2_1():
-    answer = ['Discrete', 'Quantitative', 'ratio']
+    answer = ['discrete', 'quantitative', 'ratio']
     return answer
 
 def question2_2():
-    answer = ['Continuous', 'Quantitative', 'ratio']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer
 
 def question2_3():
-    answer = ['Continuous', 'Quantitative', 'ratio']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer 
 
 def question2_4():
-    answer = ['Discrete', 'Qualitative', 'Ordinal']
+    answer = ['discrete', 'qualitative', 'ordinal']
     return answer
 
 def question2_5():
-    answer = ['Binary', 'Qualitative', 'Nominal']
+    answer = ['discrete', 'qualitative', 'nominal']
     return answer
